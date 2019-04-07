302af90a8a93d47eb21812fea28c52ced0464843
himanshu <himanshu@mail.exlmia.io>
2019-04-07 09:09:38 +0000

    Server Implementation using SpaceX mongodb API

---
+++ server/index.js
@@ -0,0 +1,134 @@
+const gql = require("graphql-tag");
+const { ApolloServer } = require("apollo-server");
+const { getDB  } = require("./db");
+
+const typeDefs = gql`
+  # Entry Points
+  type Query {
+    launches: [Launch]
+    launch(id: Int!): Launch
+  }
+
+  # Types
+  type Launch {
+    flight_number: Int!
+    mission_name: String!
+    details: String
+    links: LaunchLinks
+    rocket: LaunchRocket
+  }
+  type LaunchLinks {
+    flickr_images: [String]
+  }
+  type LaunchRocket {
+    rocket_name: String!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    launches: async (obj, args, context) => {
+      const data = await context.db
+        .collection("launch")
+        .find()
+        .limit(10)
+        .toArray();
+      return data;
+    },
+    launch: async (obj, { flight_number  }, context) => {
+      const [data] = await context.db
+        .collection("launch")
+        .find({ flight_number  })
+        .limit(1)
+        .toArray();
+      return data;
+    }
+  }
+};
+
+/*
+const resolvers = {
+  Query: {
+    launches: (obj, args, context) => launches,
+    launch: (obj, args, context) =>
+      launches.find(launch => args.id === launch.flight_number)
+  }
+};
+*/
+
+(async () => {
+  const db = await getDB();
+
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: { db  }
+  });
+
+  server.listen({port:4201}).then(({ url  }) => console.log(`🚀 Server ready at ${url}`));
+})();
+
+/*
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
+server.listen({ port: 4201}).then(({ url  }) => console.log(`🚀 Server ready at: ${url}`));
+
+const launches = [
+  {
+    flight_number: 74,
+    mission_name: "Iridium NEXT Mission 8",
+    details: `SpaceX's first flight of 2019 will be the eighth and final launch of its planned Iridium flights. Delivering 10 satellites to low earth orbit, this brings the total up to 75 and completes the Iridium NEXT constellation. This mission launches from SLC-4E at Vandenberg AFB. The booster is expected to land on JRTI.`,
+    links: {
+      flickr_images: [
+        "https://farm5.staticflickr.com/4866/39745612523_14270b4b9d_o.jpg"
+      ]
+    },
+    rocket: {
+      rocket_name: "Falcon 9"
+    }
+  },
+{
+    flight_number: 73,
+    mission_name: "GPS III SV01",
+    details: `SpaceX's twenty-first flight of 2018 launched the first of the new GPS III satellites (Block IIIA) for the United States Air Force and was SpaceX's first EELV mission. The spacecraft was delivered to a MEO transfer orbit from SLC-40 at Cape Canaveral Air Force Station. This mission was the first to fly with the redesigned COPV on the first stage (B1054) as well as the second. The booster was expended.`,
+    links: {
+      flickr_images: [
+        "https://farm5.staticflickr.com/4864/45715171884_f1dd88c058_o.jpg"
+      ]
+    },
+    rocket: {
+      rocket_name: "Falcon 9"
+    }
+  },
+{
+    flight_number: 72,
+    mission_name: "CRS-16",
+    details: `SpaceX's 16th Crew Resupply Mission on behalf of NASA, with a total of 20 contracted flights. This will bring essential supplies to the International Space Station using SpaceX's reusable Dragon spacecraft. The Falcon 9 will launch from SLC-40 at Cape Canaveral Air Force Station. During the landing of the first stage, a grid fin hydraulic pump stalled, causing the core to enter an uncontrolled roll, and resulting in a (succesful) water landing.`,
+    links: {
+      flickr_images: [
+        "https://farm5.staticflickr.com/4835/45473442624_69ee8bee45_o.jpg"
+      ]
+    },
+    rocket: {
+      rocket_name: "Falcon 9"
+    }
+  },
+{
+    flight_number: 71,
+    mission_name: "SSO-A",
+    details: `SpaceX's nineteenth flight of 2018 will fly SSO-A: SmallSat Express out of Vandenberg SLC-4E for Spaceflight. SSO-A is a rideshare to sun synchronus low earth orbit consisting of 64 individual microsatellites and cubesats. It is also likely to be the third flight of core B1046 which previously flew Bangabandhu-1 and Merah Putih. If this happens it will be the first time a Falcon 9 has flown more than two missions. `,
+    links: {
+      flickr_images: [
+        "https://farm5.staticflickr.com/4875/45257565145_d53757e0b2_o.jpg"
+      ]
+    },
+    rocket: {
+      rocket_name: "Falcon 9"
+    }
+  }
+];
+*/
+
